340c1a7970ad6f189099b4eb6e8392a9a5bb2b74
hichemchems <hichem.djacta@gmail.com>
2025-10-25 00:28:29 +0200

    Remove duplicate backend folder for o2switch deployment

--- backend/server.js
+++
@@ -1,3 +0,0 @@
-// For Passenger deployment, use app.js instead
-// This file is kept for local development and Docker
-require('./app.js');
--- backend/socket.js
+++
@@ -1,9 +0,0 @@
-let io;
-
-const setIo = (ioInstance) => {
-  io = ioInstance;
-};
-
-const getIo = () => io;
-
-module.exports = { setIo, getIo };
